eacb5a239a7d85094d2565a36358dc7a8db25692
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-06-25 14:17:20 -0400

    WIP Carousel

--- script/carousel.js
+++ script/carousel.js
@@ -7,13 +7,13 @@ const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-//Arrange the slide next to one another
+// //Arrange the slide next to one another
 const setSlidePosition = (slide, index) => {
     slide.style.left = slideWidth * index + 'px';
 };
 
 slides.forEach(setSlidePosition);
-
+// <----------------------------------------------------------------FUNCTIONS
 const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
     currentSlide.classList.remove('current-slide');
@@ -25,20 +25,44 @@ const updateDots = (currentDot, targetDot) => {
     targetDot.classList.add("current-slide");
 }
 
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
 //when I click left, move slides to the left
 prevButton.addEventListener("click", e => {
     const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
 
     moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides,prevButton, nextButton, prevIndex);
 });
 
 //when I click right, move slides to the right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
     //move to the next slide
    moveToSlide(track, currentSlide, nextSlide);
+   updateDots(currentDot, nextDot);
+   hideShowArrows(slides,prevButton, nextButton, nextIndex);
 })
 
 //when I click the nac indicators, move to that slide.
@@ -55,4 +79,5 @@ dotsNav.addEventListener('click', e => {
 
     moveToSlide(track, currentSlide, targetSlide);
     updateDots(currentDot, targetDot);
-})
+    hideShowArrows(slides,prevButton, nextButton, targetIndex);
+});
