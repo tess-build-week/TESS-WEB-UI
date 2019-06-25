563f4cbd23de8ca18a63d4e2e32d973ace531803
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-06-24 20:16:12 -0400

    TESS WEB UI Carousel completed functionality, added event listener to the Nav Dots

--- script/carousel.js
+++ script/carousel.js
@@ -8,31 +8,51 @@ const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 //Arrange the slide next to one another
-// slides[0].style.left = slideWidth * 0 + "px";
-// slides[1].style.left = slideWidth * 1 + "px";
-// slides[2].style.left = slideWidth * 2 + "px";
-
 const setSlidePosition = (slide, index) => {
     slide.style.left = slideWidth * index + 'px';
 };
 
 slides.forEach(setSlidePosition);
-    
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove("current-slide");
+    targetDot.classList.add("current-slide");
+}
 
 //when I click left, move slides to the left
+prevButton.addEventListener("click", e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
 
+    moveToSlide(track, currentSlide, prevSlide);
+});
 
 //when I click right, move slides to the right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
     //move to the next slide
-    track.style.transform = 'translateX(-' + amountToMove + ')';
-    currentSlide.classList.remove('current-slide');
-    nextSlide.classList.add('current-slide');
-
+   moveToSlide(track, currentSlide, nextSlide);
 })
 
-
 //when I click the nac indicators, move to that slide.
+dotsNav.addEventListener('click', e => {
+    //what indicator was clicked on
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector(".current-slide");
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+})
