fc93039dbe626e414cb280832b9f7c9336a0a140
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-06-27 20:39:21 -0400

    Unable to make swipe effect for carousel, removed js file for it, and content related to it on HTML

--- script/carousel-mobile.js
+++
@@ -1,7 +0,0 @@
-//Carousel picture swiping for mobile
-
-// document.querySelector('.carousel_track-container').addEventListener('touchstart', f);
-
-// function f (e){
-//     console.log(e.touches);
-// }
